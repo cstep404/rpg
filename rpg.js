b3afeda75dfe2520321ed44304a571181b1b33c5
Chip <cstep772@gmail.com>
2023-07-08 17:51:38 -0400

    Reworked class selection loop and added Witch Hunter base stats

--- rpg.js
+++ rpg.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 
 // function that provides the starting game menu
 const startMenu = () => {
-    console.log(`Welcome! Please select from one of the following options:
+    console.log(`Welcome to Simple RPG! Please select from one of the following options:
     1. Start game
     2. Quit
     `);
@@ -28,7 +28,7 @@ console.log("user chose start game, game is continuing");
 // ask the user their characters name
 let characterName = prompt("What is your character's name?: ")
 
-console.log(characterName);
+console.log(`Your characters name is: ${characterName}`);
 
 // choose class - currently only 1 class: witch hunter. others will be added later
 const classMenu = () => {
@@ -43,16 +43,45 @@ const classMenu = () => {
 
 let characterClass;
 
-// loop until user makes a valid class choice
+// loop until user makes a valid class choice and assign it to the characterClass var
 while (true) {
     classMenu();
     let classChoice = prompt("Enter your choice: ");
     if (classChoice === "1") {
         characterClass = "Witch Hunter";
         break;
+    } else if (classChoice === "2") {
+        characterClass = "Barbarian";
+        break;
+    } else if (classChoice === "3") {
+        characterClass = "Warlock";
+        break;
+    } else if (classChoice === "4") {
+        characterClass = "Paladin";
+        break;
+    } else if (classChoice === "5") {
+        characterClass = "Shaman";
+        break;
     } else {
-        console.log("Please make a valid choice.");
+        console.log("Please make a valid selection.");
     }
 }
 
-console.log(`Player has chosen the ${characterClass} class!`)
+characterStats = {
+    "name": characterName,
+    "class": characterClass
+}
+
+// assign basic stats based on class
+if (characterStats["class"] === "Witch Hunter") {
+    characterStats.vitality = 10;
+    characterStats.strength = 10;
+    characterStats.wisdom = 10;
+    characterStats.agility = 10;
+}
+
+console.log(`It has been written: you are ${characterStats["name"]} the ${characterStats["class"]}.`);
+console.log("Prepare yourself...")
+
+console.log("Current character stats: ");
+console.log(characterStats);
